d73d0a8769a2551e1e655f72075098049b3596be
yujun <clf0914@naver.com>
2025-04-02 12:44:50 +0900

    loginController RequestMapping update

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/login")
+@RequestMapping("/api")
 public class LoginController {
 
     private final LoginService loginService;

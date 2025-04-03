5a2c3f9d711b391bbae155fdbd69bfc38610cb46
cheog0 <clf0914@naver.com>
2025-04-03 18:51:16 +0900

    #LoginRepository Update

--- src/main/java/com/example/demo/repository/LoginRepository.java
+++ src/main/java/com/example/demo/repository/LoginRepository.java
@@ -4,7 +4,9 @@ import com.example.demo.entity.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Long> {
-    Login findByUserid(String userid);
+    Optional<Login> findByUserid(String userid);
 }
